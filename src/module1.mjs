f1b985042a8c7ee7720484c7dc3bafaa94b81fb6
Kaushal <kaushalkawade07@gmail.com>
2023-10-16 12:00:19 +0530

    Navbar as component is created in react

--- src/module1.mjs
+++
@@ -1,9 +0,0 @@
-// make sure that run module1.mjs after reaching correct path 
-import ui,{a,b,d} from './module2.mjs'
-//import ui,{a1,b2,d1} from './module2.mjs' this is wrong
-// we can not change naming of variables here
-// names of variables must be same as inside module 2 file
-
-console.log(ui)
-console.log(b)
-console.log(d)
--- src/module2.mjs
+++
@@ -1,9 +0,0 @@
-const a = 'Kaushal'
-const b = 'Suraj'
-const c = 'Om'
-const d = 'Aditya'
-
-export default c;
-export {a};
-export {b};
-export {d};
