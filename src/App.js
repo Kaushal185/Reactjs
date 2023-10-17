5ddbc7a13a3c9be991e738d7767e9bf873092068
Kaushal <kaushalkawade07@gmail.com>
2023-10-18 00:33:44 +0530

    props and propTypes done

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar';
-
+import TextForm from './components/TextForm';
 function App() {
   return (
     //props are used for reuse components with variable values.
     // example here component Navbar is used with title name amazon.com
+    <>
+    // App() return only single component while returning bunch them all inside single entity.
     // we can reuse our components again and again
     <Navbar title = "amazon.com" about = "About"/>
     
     // <Navbar/> This is Navbar having default tile and about values
+    <TextForm/>
+    </>
   );
 }
 
