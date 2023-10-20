4141d5f999df4656daab6f13b147d9a3809f811f
Kaushal <kaushalkawade07@gmail.com>
2023-10-20 11:59:55 +0530

    basics of hooks and events

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ function App() {
     //props are used for reuse components with variable values.
     // example here component Navbar is used with title name amazon.com
     <>
-    // App() return only single component while returning bunch them all inside single entity.
-    // we can reuse our components again and again
+     {/* App() return only single component while returning bunch them all inside single entity.
+    we can reuse our components again and again */}
     <Navbar title = "amazon.com" about = "About"/>
     
-    // <Navbar/> This is Navbar having default tile and about values
-    <TextForm/>
+    {/* // <Navbar/> This is Navbar having default tile and about values */}
+    <TextForm heading = "Enter Text Here"/>
     </>
   );
 }
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -1,11 +1,26 @@
-import React from 'react'
-
+import React, { useState } from 'react'
 export default function TextForm(props) {
+  const handleUpClick = ()=>{
+    console.log("Uppercase was clicked");
+  }
+  const handleOnChange = (event)=>{
+    //In this method we used event handling mechanism
+    // for each text written in textarea the event is triggered and it perform action inside this function.
+    // the event used here is onChange
+    console.log("handle on change called");
+    setText(event.target.value);
+    console.log(text);
+  }
+  const[text,setText] = useState('Enter text here');
+  // text = 'new Text'; wrong way to change text
+  // setText('new Text'); correct way
   return (
-    <div class="mb-3">
-        <h1>{props.heading}</h1>
-        <label for="exampleInputEmail1" className="form-label">Email address</label>
-        <div id="emailHelp" className="form-text">We'll never share your email with anyone else.</div>
+   <div>  
+     <h1>{props.heading}</h1>
+    <div className ="mb-3">
+      <textarea className = "form-control" id = "myBox" onChange={handleOnChange} value={text} rows = "8"></textarea>
+    </div>
+    <button className = "btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
     </div>
   )
 }
